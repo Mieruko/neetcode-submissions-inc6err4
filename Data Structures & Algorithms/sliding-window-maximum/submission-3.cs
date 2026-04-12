6c10c807737d268ca3904e11d29cba2cd4f57298
Mieruko <111077174+Mieruko@users.noreply.github.com>
2026-04-12 16:22:08 +0700

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        int n = nums.Length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = nums[0];
+        rightMax[n-1] = nums[n-1];
+
+        for(int i=1;i<n;i++){
+            if(i % k == 0){
+                leftMax[i] = nums[i];
+            } else{
+                leftMax[i] = Math.Max(leftMax[i-1],nums[i]);
+            }
+
+            if((n-1-i) % k == 0){
+                rightMax[n-1-i] = nums[n-1-i];
+            } else{
+                rightMax[n-1-i] = Math.Max(rightMax[n-i], nums[n-1-i]);
+            }
+        }
+
+        int[] res  = new int[n+1-k];
+        for(int i=0;i<n-k+1;i++){
+            res[i] = Math.Max(leftMax[i+k-1],rightMax[i]);
+        }
+
+        return res;
+    }
+}
