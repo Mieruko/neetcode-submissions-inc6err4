e35e1f8dbc0fa996de90f11d3402c15b6aa2d519
Mieruko <111077174+Mieruko@users.noreply.github.com>
2026-04-21 16:02:26 +0700

    Add: valid-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-0.cs
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool IsValidBST(TreeNode root) {
+        return Dfs(root, long.MinValue , long.MaxValue);
+    }
+
+    public bool Dfs(TreeNode node , long left, long right){
+        if(node == null) return true;
+
+        if(!(left < node.val && node.val < right)) return false;
+
+        return Dfs(node.left, left, node.val) && Dfs(node.right, node.val, right);
+    }
+}
