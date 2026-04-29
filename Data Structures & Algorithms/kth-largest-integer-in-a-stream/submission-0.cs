53c23f12d4f8c10c28ac5c592aec527d62052805
Mieruko <111077174+Mieruko@users.noreply.github.com>
2026-04-29 13:16:56 +0700

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cs
@@ -0,0 +1,22 @@
+public class KthLargest {
+    private readonly int _k;
+    private readonly PriorityQueue<int, int> _minHeap;
+    public KthLargest(int k, int[] nums) {
+        _k = k;
+        _minHeap = new PriorityQueue<int, int>();
+
+        foreach (var num in nums) {
+            Add(num);
+        }
+    }
+    
+    public int Add(int val) {
+        _minHeap.Enqueue(val, val);
+
+        if (_minHeap.Count > _k) {
+            _minHeap.Dequeue();
+        }
+
+        return _minHeap.Peek();
+    }
+}
